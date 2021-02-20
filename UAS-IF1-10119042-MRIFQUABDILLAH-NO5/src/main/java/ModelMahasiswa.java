beb1bcc70a75bf972ceb90bf6d6ac41f376399c6
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2021-02-20 13:27:08 +0700

    Add files via upload

---
+++ UAS-IF1-10119042-MRIFQUABDILLAH-NO5/src/main/java/ModelMahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Rifqu Abdillah      
+ * Nama  : Muhammad Rifqu Abdillah
+ * Kelas : IF-1
+ * NIM   : 10119042
+ */
+public class ModelMahasiswa {
+    String nama;
+    String nim;
+    
+    void Mahasiswa(){
+        System.out.println("Nim :  ". concat(nim));
+        System.out.println("Nama :  ". concat(nama).concat("\n"));
+    }
+             
+}
---
+++ UAS-IF1-10119042-MRIFQUABDILLAH-NO5/src/main/java/UASPBO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Muhammad Rifqu Abdillah      
+ * Nama  : Muhammad Rifqu Abdillah
+ * Kelas : IF-1
+ * NIM   : 10119042
+ */
+public class UASPBO {
+    
+    public static void main(String[] args){
+        ModelMahasiswa modelmahasiswa1 = new ModelMahasiswa();
+        modelmahasiswa1.nim = "10110269";
+        modelmahasiswa1.nama= "Rizki Adam Kurniawan";
+        
+        
+        
+        modelmahasiswa1.Mahasiswa();
+    }
+    
+}
